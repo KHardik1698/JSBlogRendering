b015b0fe9b90e032cae9718962294d35d3ec295c
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-06 01:36:13 +0530

    Modified main.js to take data from the Axios Array
    Previously the data was taken from data.js which is a static file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { blogs } from "./data.js";
+// import { blogs } from "./data.js";
+import { blogData } from "./data-fetch.js";
 import { blogImage } from "./components/blogImage.js";
 import { blogContent } from "./components/blogContent.js";
 import { relatedLinks } from "./components/relatedLinks.js";
@@ -6,11 +7,11 @@ import { relatedLinks } from "./components/relatedLinks.js";
 window.onload = () => {
   // this function runs when window finishes loading
   const blogDiv = document.getElementById("root");
-  const blogImageDiv = blogImage(blogs[0].imageUrl);
+  const blogImageDiv = blogImage(blogData[0].imageUrl);
   blogDiv.appendChild(blogImageDiv);
-  const blogContentDiv = blogContent(blogs[0].title, blogs[0].content);
+  const blogContentDiv = blogContent(blogData[0].title, blogData[0].content);
   blogDiv.appendChild(blogContentDiv);
   const listDiv = document.getElementById("related-links");
-  const relatedLinksList = relatedLinks(blogs[0].links);
+  const relatedLinksList = relatedLinks(blogData[0].links);
   listDiv.appendChild(relatedLinksList);
 };

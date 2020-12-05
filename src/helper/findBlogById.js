b117ffe09d26ba6d347a57b0994873d7192f55af
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-06 01:40:03 +0530

    Added other ways to match & find for Async JS Functions
    Currently committed, added optional way to return matched object for Axios Promise

--- src/helper/findBlogById.js
+++ src/helper/findBlogById.js
@@ -1,7 +1,17 @@
-import { blogs } from "../data.js";
+// import { blogs } from "../data.js";
+import { blogData } from "../data-fetch.js";
+// import { getBlogsAxios, getBlogsFetch } from "../data-fetch.js";
 
 export const findBlogById = (blogId) => {
-  return blogs.filter((blog) => {
+  return blogData.filter((blog) => {
     return blog.id == blogId;
   });
 };
+
+// export const findBlogById = (blogId) => {
+//   getBlogsAxios.then((blogs) => {
+//     return blogs.filter((blog) => {
+//       return blog.id == blogId;
+//     });
+//   });
+// };

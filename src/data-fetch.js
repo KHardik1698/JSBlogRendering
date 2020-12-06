d12a3fa14f78f0b5a0f3627ef709f82ce4104b7a
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-06 16:42:44 +0530

    Removed commented codes which are not needed

--- src/data-fetch.js
+++ src/data-fetch.js
@@ -1,54 +1,19 @@
 const url = "https://niravkpatel28.github.io/json-data-server/blogs/blogs.json";
 
 export let blogData = [];
-// console.log("Blog Data", blogData);
 
 const getBlogsAxios = (async () => {
   let data;
   await axios
     .get(url)
     .then(function (response) {
-      //   console.log(response);
       data = response.data;
       data.forEach((val) => {
         blogData.push(val);
       });
-      //   console.log("Received List", data);
     })
     .catch(function (error) {
       console.log(error);
     });
   return data;
 })();
-
-// export const getBlogsAxios = (async () => {
-//   let response = await axios.get(url);
-//   return response.data;
-// })();
-
-// const getBlogsFetch = (async () => {
-//   let resData;
-//   await fetch(url)
-//     .then((response) => {
-//       // console.log(response);
-//       return response.json();
-//     })
-//     .then((data) => {
-//       //   console.log("Received List", data);
-//       resData = data;
-//       resData.forEach((val) => {
-//         blogData.push(val);
-//       });
-//     })
-//     .catch((err) => {
-//       console.log("Error", err);
-//     });
-//   return resData;
-// })();
-
-// export const getBlogsFetch = (async () => {
-//   let response = await fetch(url).then((response) => {
-//     return response.json();
-//   });
-//   return response;
-// })();
--- src/main.js
+++ src/main.js
@@ -1,27 +1,9 @@
-// import { blogs } from "./data.js";
 import { blogData } from "./data-fetch.js";
-// import { getBlogsAxios, getBlogsFetch } from "./data-fetch.js";
 import { blogImage } from "./components/blogImage.js";
 import { blogContent } from "./components/blogContent.js";
 import { relatedLinks } from "./components/relatedLinks.js";
 
 window.onload = () => {
-  // this function runs when window finishes loading
-  // getBlogsAxios.then((blogs) => {
-  //   // console.log(blogs);
-  //   const blogDiv = document.getElementById("root");
-  //   const blogImageDiv = blogImage(blogs[0].imageUrl);
-  //   blogDiv.appendChild(blogImageDiv);
-  //   const blogContentDiv = blogContent(blogs[0].title, blogs[0].content);
-  //   blogDiv.appendChild(blogContentDiv);
-  //   const listDiv = document.getElementById("related-links");
-  //   const relatedLinksList = relatedLinks(blogs[0].links);
-  //   listDiv.appendChild(relatedLinksList);
-  // });
-  // console.log(getBlogsFetch);
-  // getBlogsFetch.then((blogs) => {
-  //   console.log(blogs);
-  // });
   const blogDiv = document.getElementById("root");
   const blogImageDiv = blogImage(blogData[0].imageUrl);
   blogDiv.appendChild(blogImageDiv);
